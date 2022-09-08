ab5a9abadf937f13fbfd1a5d9cab56ba3980ba88
Sambajela <bajelasamuel021@gmail.com>
2022-09-08 12:38:54 -0700

    4-puts.c alx-low_level_programming fix 3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
-/* this is a program to print
+/**
+ * Description: this is a program to print
  * Programming is like building a multilingual puzzle, followed by a new line.
  */
-int main()
+int main(void)
 {
-	char string_constant[] = "Programming is like building a multilingual puzzle";
+	char string_constant[] = ""Programming is like building a multilingual puzzle";
 
 	puts(string_constant);
 
-	return 0;
+	return (0);
 }
