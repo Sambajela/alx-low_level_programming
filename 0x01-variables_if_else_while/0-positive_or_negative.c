6d4a1a315af942f65e4ff939e6d6b2ea9e85a36e
Sambajela <bajelasamuel021@gmail.com>
2022-09-09 08:59:38 -0700

    0-positive_or_negative.c alx-low_level_programming fix5

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,12 +5,14 @@
 /* more headers got there */
 
 /* betty style doc for function main goes there */
+
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
 	{
 		printf("%i is positive\n", n);
