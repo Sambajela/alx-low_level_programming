54edd4efbc1f27047993bf292d9d919321135af2
Sambajela <bajelasamuel021@gmail.com>
2022-09-08 18:47:28 -0700

    101-quote.c alx-low_level_programming fix 2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  * Description: means to print text
  * Return: 1
  */
-int main(void)
+int main(STDOUT_FILENO)
 {
 	fwrite(STDOUT_FILENO, "and that piece of art is useful\"
 			- Dora Korpar, 2015-10-19\n", 59);
