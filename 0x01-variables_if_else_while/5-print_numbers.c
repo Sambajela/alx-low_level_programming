ce2fb2545929f10c0a3b4b461431f2aba7f35257
Sambajela <bajelasamuel021@gmail.com>
2022-09-10 08:20:43 -0700

    5-print_numbers.c alx-low_level_programming

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - print numbers from 0-10
+ *
+ * Description: use main function
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	printf("\n");
+	return (0);
+}
