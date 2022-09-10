db0e43b9029955dc1681ae6f9ff8c6f8e744b05c
Sambajela <bajelasamuel021@gmail.com>
2022-09-10 12:31:34 -0700

    100-print_comb3.c alx-low_level_programming

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,32 +1,28 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
- * main - Prints all possible combinations of two different digits.
+ * main - combination
  *
- * Description: use main function
+ * Description: prints all possible combination of two digits
  * Return: 0
  */
-
 int main(void)
 {
-
 	int digit1, digit2;
-	
-	for (digit1 = 0; digit1 < 9; digit1++)
+
+	for (digit1 = 0; digit < 9; digit1++)
 	{
 		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{
 			putchar((digit1 % 10) + '0');
 			putchar((digit2 % 10) + '0');
-			
-			if (digit1 == 8 && digit2 == 9)
-				continue;
-																								
-			putchar(',');
-			putchar(' ');										
-		}				
-	}
 
+			if (digit == 8 && digit2 == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
 	putchar('\n');
 	return (0);
 }
