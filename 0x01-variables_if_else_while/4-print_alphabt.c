ef1d977dac6c750d89c0773fd599002a49a4343e
Sambajela <bajelasamuel021@gmail.com>
2022-09-10 08:07:14 -0700

    4-print_alphabt.c alx-low_level_programming

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - print lower case without e and q
+ *
+ * Description: use mani function
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
