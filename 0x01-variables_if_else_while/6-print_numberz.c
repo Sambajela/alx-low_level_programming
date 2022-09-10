221d04376800729b87d6654dda2d1823c5aebd3f
Sambajela <bajelasamuel021@gmail.com>
2022-09-10 08:29:47 -0700

    6-print_numberz.c alx-low_level_programming

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - print numbers from 0-10
+ *
+ * Description: use main function
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n < '9'; n++)
+	{
+		putchar(n);
+	}
+	putchar("\n");
+	return (0);
+}
