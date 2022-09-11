0a3702f39b4af48a03cdb5fd8a5160c778e22899
Sambajela <bajelasamuel021@gmail.com>
2022-09-11 15:14:07 -0700

    102-print_comb5.c alx-low_level_programming

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,7 +13,10 @@ int main(void)
 {
 	int i, j, k, l;
 
-	i = j = k = 48; l = 49;
+	i = 48;
+	j = 48;
+	k = 48;
+	l = 49;
 
 	while ((i < 58))
 	{
@@ -35,15 +38,20 @@ int main(void)
 			{
 				if (j < 56)
 				{
-					l = ++j + 1; k = i;
+					l = ++j + 1;
+					k = i;
 				}
 				else if (j == 56)
 				{
-					j++; k = i + 1; l = 48;
+					j++;
+					k = i + 1;
+					l = 48;
 				}
 				else if (j == 57)
 				{
-					j = 48; l = 49; k = ++i;
+					j = 48;
+					l = 49;
+					k = ++i;
 				}
 			}
 			else if (l < 57)
@@ -52,7 +60,8 @@ int main(void)
 			}
 			else
 			{
-				l = 48; k++;
+				l = 48;
+				k++;
 			}
 		}
 	}
