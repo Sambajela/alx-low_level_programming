739d86bd4b2cfb4500e63732e9d9da0f51ebefca
Sambajela <bajelasamuel021@gmail.com>
2022-09-14 12:41:20 -0700

    10-add.c alx-low_level_programming

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - add two integers
+ * @num1: first integer
+ * @num2: second integer
+ * Return: return addition result
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
