696e75213ed3f40511a6c7c8b7dca2761cca2a84
Sambajela <bajelasamuel021@gmail.com>
2022-09-14 08:38:21 -0700

    5-sign.c alx-low_level_programming

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - function
+ *
+ * @n: int for argument
+ * Return: 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
