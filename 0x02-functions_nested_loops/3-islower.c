caa707c377f19441b7fa9eb93172a5e99c62a9e5
Sambajela <bajelasamuel021@gmail.com>
2022-09-14 07:04:41 -0700

    3-islower.c alx-low_level_programming

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _islower - to check for lowercase alphabet
+ *
+ * Description: to print 1 if true 0 if false
+ * Return: 0
+ */
+int _islower(int c)
+{
+	if (c >= 'a'; && c <= 'z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
