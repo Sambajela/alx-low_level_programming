ff4f3385d01000be65c6cd91db9b530d7a24b8a3
Sambajela <bajelasamuel021@gmail.com>
2022-09-14 06:53:54 -0700

    2-print_alphabet_x10.c alx-low_level_programming

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print 10xalphabet
+ *
+ * Description: use main
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char c, i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
