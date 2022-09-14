5f1f6f6442c92596caf14df5a04febfb2acaa1b1
Sambajela <bajelasamuel021@gmail.com>
2022-09-14 14:33:51 -0700

    101-natural.c alx-low_level_programming

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * main - counts multiples of 3 and 5 lesser 1024
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	int n, sum = 0;
+
+	for (n = 0; n < 1024; n++)
+	{
+		if ((n % 3) == 0 || (n % 5) == 0)
+			sum += n;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
