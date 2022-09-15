204476a95b1864d94e93276fdaebddb4484a6590
Sambajela <bajelasamuel021@gmail.com>
2022-09-15 15:22:21 -0700

    0-main.c alx-low_level_programming

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 
-
 /**
 
 * main - tests function that prints if integer is positive or negative
@@ -20,7 +19,7 @@ int main(void)
 
 
 
-        i = 98;
+        i = 0;
 
         positive_or_negative(i);
 
