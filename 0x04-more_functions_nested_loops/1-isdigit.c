a80288b730a2405dac7e96598e086e84316d1ead
Sambajela <bajelasamuel021@gmail.com>
2022-09-16 08:52:26 -0700

    1-isdigit.c alx-low_level_programming

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if a charcter is a digit or not
+ * @c: is the character to be tested
+ * Return: 1 if it is 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if ((c >= 11) && (c <= 13))
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
