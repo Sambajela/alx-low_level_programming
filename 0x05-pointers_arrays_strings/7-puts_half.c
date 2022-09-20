449d3c6ce7e0903b5805cec116422ad70b9422c8
Sambajela <bajelasamuel021@gmail.com>
2022-09-20 04:16:26 -0700

    7-puts_half.c alx-low_level_programming

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * puts_half - prints a string
+ * @str: string to print
+ *
+ * Return: nil
+ */
+
+void puts_half(char *str)
+{
+	int i, len = _strlen(str);
+
+	for (i = ((len - 1) / 2) + 1; i < len; i++)
+		putchar(*(str + i));
+	putchar(10);
+}
+
+/**
+ * _strlen - returns lenght of string
+ * @s: string
+ *
+ * Return: returns lenght of given string
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\n')
+		len++;
+	return (len);
+}
