bacd902fc36a610167bdefe2e38b352a90bebd9e
Sambajela <bajelasamuel021@gmail.com>
2022-09-27 02:19:30 -0700

    3-strspn.c alx-low_level_programming

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strspn - gets lenght of prefix substring
+ * @s: string
+ * @accept: main string
+ *
+ * Return: number of byte
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[i])
+				break;
+		}
+		if (!accept[i])
+			break;
+	}
+	return (i);
+}
