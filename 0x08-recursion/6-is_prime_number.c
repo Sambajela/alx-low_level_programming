02f5ed340a133af19e202d126ba931baf88f3476
Sambajela <bajelasamuel021@gmail.com>
2022-09-28 15:51:59 -0700

    0x08-recursion alx-low_level_programming

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,22 @@
 #include "main.h"
 
 /**
+ * is_prime - returns the 1 if n is prime
+ * @n: num to be checked
+ *
+ * Return: 1 if prime 0 if otherwise
+ *
+ */
+int is_prime(int n, int start)
+{
+	if (start <= 1)
+		return (1);
+	else if (n % start == 0)
+		return (0);
+	return (is_prime(n, start - 1));
+}
+
+/**
  * is_prime_number - checs if a number is prime
  * @n: number to be checked
  *
@@ -13,19 +29,4 @@ int is_prime_number(int n)
 	if (n <= 1)
 		return (0);
 	return (is_prime(n, start));
-}
-
-/**
- * is_prime - returns the 1 if n is prime
- * @n: num to be checked
- *
- * Return: 1 if prime 0 if otherwise
- */
-int is_prime(int n, int start)
-{
-	if (start <= 1)
-		return (1);
-	else if (n % start == 0)
-		return (0);
-	return (is_prime(n, start - 1));
 }
