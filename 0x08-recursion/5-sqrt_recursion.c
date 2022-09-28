338c726bddef8802755e8eea8653e569bd7d6d47
Sambajela <bajelasamuel021@gmail.com>
2022-09-28 14:50:30 -0700

    0x08-recursion alx-low_level_programming

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,19 +1,6 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - returns natural square root of a number
- * @n: integer
- *
- * Return: -1 if no doesnt have a natural square root
- */
-int _sqrt_recursion(int n)
-{
-	if (n == 1 || n == 0)
-		return (n);
-	return (_sqrt(0, n));
-}
-
-/**
  * _sqrt - returns the square root of a number
  * @n: test number
  * @x: squared number
@@ -27,4 +14,17 @@ int _sqrt(int n, int x)
 	else if (n * n == x)
 		return (n);
 	return (_sqrt(n + 1, x));
+}
+
+/**
+ * _sqrt_recursion - returns natural square root of a number
+ * @n: integer
+ *
+ * Return: -1 if no doesnt have a natural square root
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 1 || n == 0)
+		return (n);
+	return (_sqrt(0, n));
 }
