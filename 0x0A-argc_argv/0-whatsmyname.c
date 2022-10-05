f389b665caf9eece6ed2107726326d428ca50a62
Sambajela <bajelasamuel021@gmail.com>
2022-10-05 02:45:28 -0700

    0x0A-argv_argc alx-low_level_programming

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program followed by a new line
+ * @argc: number of arguments
+ * @argv: pointer to array of input argument
+ * Return: 0 on success
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
