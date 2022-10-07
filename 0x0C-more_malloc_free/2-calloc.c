0bae1bb2fad5d4dae00a144fcb11a4ae47b43bab
Sambajela <bajelasamuel021@gmail.com>
2022-10-07 11:55:42 -0700

    more malloc alx-low_level_programming

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,45 +2,23 @@
 #include <stdlib.h>
 
 /**
- * _calloc - allocates memory for an array
- * @nmemb: number of elements in array
- * @size: number of bytes per element
+ * _calloc - allocates memory for an array, using malloc.
+ * @nmemb: number of elements in the array
+ * @size: size in bytes of the elements
  *
- * Return: a void pointer to the allocated memory
+ * Return: void pointer to allocated memory
  */
-
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ptr = NULL;
+	char *p;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
-	{
 		return (NULL);
-	}
-
-	ptr = malloc(nmemb * size);
-	if (ptr == NULL)
+	p = malloc(nmemb * size);
+	if (p == NULL)
 		return (NULL);
-	setm_zero((char *)ptr, size * nmemb);
-
-	return (ptr);
-}
-
-
-/**
- * setm_zero - set memory on zero
- * @ptr: pointer to byte block
- * @size: number of bytes to assign a zero
- *
- * Return: void
- */
-
-void setm_zero(char *ptr, int size)
-{
-	int i = 0;
-
-	for (i = 0; i < size; i++)
-	{
-		ptr[i] = '\0';
-	}
+	for (i = 0; i < (nmemb * size); i++)
+		p[i] = 0;
+	return (p);
 }
